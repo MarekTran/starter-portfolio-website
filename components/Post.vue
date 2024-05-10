c65c489813d7e3bde90238c69dfcaac2165bb1e0
marek <marekk.tran@gmail.com>
2024-05-10 13:38:31 +0200

    Fixed Read More blog post card button placement added Dockerfile

--- components/Post.vue
+++ components/Post.vue
@@ -4,15 +4,17 @@ const props = defineProps(['posts'])
 
 <template>
   <div v-for="post in props.posts" :key="post.slug"
-    class="bg-slate-100 rounded-lg shadow-md overflow-hidden hover:opacity-75">
+    class="bg-slate-100 rounded-lg shadow-md overflow-hidden hover:opacity-75 flex flex-col">
     <NuxtLink :to="post._path">
       <img :src="`/images/blog/${post.cover}`" alt="Blog Post Cover Image" class="w-full h-48 object-cover">
     </NuxtLink>
     <div class="p-6">
       <h2 class="text-xl font-bold mb-2">{{ post.title }}</h2>
       <p class="text-gray-700 mb-4">{{ post.description }}</p>
+    </div>
+    <div class="mt-auto mb-8 px-6">
       <NuxtLink :to="post._path"
-        class="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded float-right mb-8">Read More
+        class="inline-block bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded float-right">Read More
       </NuxtLink>
     </div>
   </div>
